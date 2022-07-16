23a144807670dde2ffd2a1ef3557c133d56e390a
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-16 12:29:55 +0600

    coment out all the things

--- src/solution.js
+++ src/solution.js
@@ -25,23 +25,23 @@
 // console.log(`hello world`);
 // for google
 // st;
-function main() {
-  const readline = require("readline");
-  // const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.on("line", (line) => {
-    const data = parseInt(line);
-    if (data >= 4 && data % 2 == 0) console.log(`YES`);
-    else console.log(`NO`);
-  });
-}
+// function main() {
+//   const readline = require("readline");
+//   // const readline = require("readline");
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+//   rl.on("line", (line) => {
+//     const data = parseInt(line);
+//     if (data >= 4 && data % 2 == 0) console.log(`YES`);
+//     else console.log(`NO`);
+//   });
+// }
 
 // for codeforces
 // function main() {
 //   a = parseInt(readline());
 //   print(a % 2 === 0 && a > 2 ? "YES" : "NO");
 // }
-main();
+// main();
