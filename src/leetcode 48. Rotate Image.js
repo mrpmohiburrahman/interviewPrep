cee98ffd6cd4d302467e396f298835c90d26503a
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-17 06:48:19 +0600

    48 rotate image

---
+++ src/leetcode 48. Rotate Image.js
@@ -0,0 +1,14 @@
+var rotate = function (matrix) {
+  console.log(matrix);
+  matrix.reverse();
+  console.log(matrix);
+  for (let i = 0; i < matrix.length; i++)
+    for (let j = i + 1; j < matrix[i].length; j++)
+      [matrix[j][i], matrix[i][j]] = [matrix[i][j], matrix[j][i]];
+  console.log(matrix);
+};
+rotate([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
