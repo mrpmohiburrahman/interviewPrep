3b5497f478508c8fddaa20b1d00916a5da8c6ec4
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-29 21:31:08 +0600

    704. Binary Search

--- src/NeetCode/704. Binary Search.js
+++ src/NeetCode/704. Binary Search.js
@@ -3,14 +3,32 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {};
+var search = function (nums, target) {
+  let leftIndex = 0;
+  let rightIndex = nums.length - 1;
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (nums[middleIndex] === target) return middleIndex;
+    else if (target < nums[middleIndex]) rightIndex = middleIndex - 1;
+    else if (target > nums[middleIndex]) leftIndex = middleIndex + 1;
+  }
+  return -1;
+};
 
-search([-1, 0, 3, 5, 9, 12], 9);
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
+// search([-1, 0, 3, 5, 9, 12], 9);
 // Input: nums = [-1,0,3,5,9,12], target = 9
 // Output: 4
 // Explanation: 9 exists in nums and its index is 4
 
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
 // search([-1, 0, 3, 5, 9, 12], 2);
 // Input: nums = [-1,0,3,5,9,12], target = 2
 // Output: -1
 // Explanation: 2 does not exist in nums so return -1
+
+// Constraints:
+//     1 <= nums.length <= 104
+//     -10^4 < nums[i], target < 10^4
+//     All the integers in nums are unique.
+//     nums is sorted in ascending order.
