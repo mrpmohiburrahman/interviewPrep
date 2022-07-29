843864bcda9a4ebb52dc9a92fa0067e0d48de5b5
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-29 06:29:30 +0600

    readable mergesort

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,13 +1,17 @@
 const mergeSort = (list) => {
-  if (list.length < 2) return [list[0]];
+  if (list.length === 1) return list;
+
   const middleIndex = Math.floor(list.length / 2);
 
+  // Divide in to 2 arrays
   const leftArray = list.slice(0, middleIndex);
   const rightArray = list.slice(middleIndex);
 
+  // recursion
   const leftMergeSort = mergeSort(leftArray);
   const rightMergeSort = mergeSort(rightArray);
 
+  // merge
   return merge(leftMergeSort, rightMergeSort);
 };
 
@@ -28,13 +32,9 @@ const merge = (leftArray, rightArray) => {
       leftArrayIndex++;
     }
   }
-  if (rightArrayIndex > leftArrayIndex) {
-    mergedArray.push(...leftArray.slice(leftArrayIndex));
-  } else if (leftArrayIndex > rightArrayIndex) {
-    mergedArray.push(...rightArray.slice(rightArrayIndex));
-  }
-
-  return mergedArray;
+  return mergedArray
+    .concat(leftArray.slice(leftArrayIndex))
+    .concat(rightArray.slice(rightArrayIndex)); // concat (joins) two or more arrays, returns a new array, containing the joined arrays, does not change the existing arrays.
 };
 
 // mergeSort([5, 6, 2, 1, 3]);
@@ -46,3 +46,11 @@ console.log(mergeSort([9, 8, 7, 6, 5, 4, 3, 2, 1]));
 // mergeSort([4, 3, 2, 1]);
 // mergeSort([1, 2, 3, 4]);
 // mergeSort([2, 1]);
+
+// if (rightArrayIndex > leftArrayIndex) {
+//   mergedArray.push(...leftArray.slice(leftArrayIndex));
+// } else if (leftArrayIndex > rightArrayIndex) {
+//   mergedArray.push(...rightArray.slice(rightArrayIndex));
+// }
+
+// return mergedArray;
