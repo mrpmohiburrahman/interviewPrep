b5a542f67531b9b8491bb0093448706e0234011c
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-29 21:07:29 +0600

    20. Valid Parentheses using hashMap

--- src/NeetCode/20. Valid Parentheses.js
+++ src/NeetCode/20. Valid Parentheses.js
@@ -2,22 +2,45 @@
  * @param {string} s
  * @return {boolean}
  */
+// using hashmap
+// time complexity: O(n)
+// space complexity: O(n)
 var isValid = function (s) {
-  const stack = [s[0]];
-  for (let index = 1; index < s.length; index++) {
-    if (s[index] === ")" && stack[stack.length - 1] === "(") {
-      stack.pop();
-    } else if (s[index] === "}" && stack[stack.length - 1] === "{") {
-      stack.pop();
-    } else if (s[index] === "]" && stack[stack.length - 1] === "[") {
-      stack.pop();
-    } else {
-      stack.push(s[index]);
-    }
+  if (!s) return false;
+  const closeMap = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  const stack = [];
+  for (let index = 0; index < s.length; index++) {
+    const curItem = s[index];
+    if (closeMap[curItem]) {
+      const topElement = stack.length === 0 ? "#" : stack.pop();
+      if (topElement !== closeMap[curItem]) return false;
+    } else stack.push(s[index]);
   }
-  if (stack.length) return false;
-  return true;
+  return stack.length === 0;
 };
+
+// time complexity: O(n)
+// space complexity: O(n)
+// var isValid = function (s) {
+//   const stack = [s[0]];
+//   for (let index = 1; index < s.length; index++) {
+//     if (s[index] === ")" && stack[stack.length - 1] === "(") {
+//       stack.pop();
+//     } else if (s[index] === "}" && stack[stack.length - 1] === "{") {
+//       stack.pop();
+//     } else if (s[index] === "]" && stack[stack.length - 1] === "[") {
+//       stack.pop();
+//     } else {
+//       stack.push(s[index]);
+//     }
+//   }
+//   if (stack.length) return false;
+//   return true;
+// };
 console.log(isValid("{[]}"));
 // console.log(isValid("()"));
 // isValid("()");
