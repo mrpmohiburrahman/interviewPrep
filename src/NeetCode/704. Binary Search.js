fc987d22ae56b0e3f10a13511dced244ba4b6ffa
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-29 21:10:38 +0600

    704. Binary Search bare

---
+++ src/NeetCode/704. Binary Search.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {};
+
+search([-1, 0, 3, 5, 9, 12], 9);
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+// search([-1, 0, 3, 5, 9, 12], 2);
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
