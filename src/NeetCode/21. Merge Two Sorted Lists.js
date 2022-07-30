4ccf59a038ef9b8e1f8c04d72f51866a5680f557
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-30 16:01:35 +0600

    21. Merge Two Sorted Lists

--- src/NeetCode/21. Merge Two Sorted Lists.js
+++ src/NeetCode/21. Merge Two Sorted Lists.js
@@ -10,7 +10,22 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {};
+var mergeTwoLists = function (list1, list2) {
+  const nullNode = { val: 0, next: null };
+  let prev = nullNode;
+  while (list1 && list2) {
+    if (list1.val >= list2.val) {
+      prev.next = list2;
+      list2 = list2.next;
+    } else {
+      prev.next = list1;
+      list1 = list1.next;
+    }
+    prev = prev.next;
+  }
+  prev.next = list1 || list2;
+  return nullNode.next;
+};
 
 //  Input: list1 = [1,2,4], list2 = [1,3,4]
 // Output: [1,1,2,3,4,4]
