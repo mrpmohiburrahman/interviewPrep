ed188b09e23ab14312ea39ed70eba36c12add9bf
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-30 15:48:28 +0600

    21. Merge Two Sorted Lists initial

---
+++ src/NeetCode/21. Merge Two Sorted Lists.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {};
+
+//  Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
