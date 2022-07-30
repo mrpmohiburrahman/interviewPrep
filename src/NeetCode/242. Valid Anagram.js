12556f59b919d9388487ce1e5b4d77d1a0db04ed
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-07-30 13:49:20 +0600

    242. Valid Anagram using hashMap

--- src/NeetCode/242. Valid Anagram.js
+++ src/NeetCode/242. Valid Anagram.js
@@ -3,15 +3,39 @@
  * @param {string} t
  * @return {boolean}
  */
+
 var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
-  const sortedS = [...s].sort();
-  const sortedT = [...t].sort();
-  for (let index = 0; index < sortedS.length; index++) {
-    if (sortedS[index] !== sortedT[index]) return false;
+  let sMap = {};
+  let tMap = {};
+  for (let index = 0; index < s.length; index++) {
+    if (sMap.hasOwnProperty(s[index])) {
+      sMap[s[index]]++;
+    } else {
+      sMap[s[index]] = 1;
+    }
+    if (tMap.hasOwnProperty(t[index])) {
+      tMap[t[index]]++;
+    } else {
+      tMap[t[index]] = 1;
+    }
+  }
+  for (const key in sMap) {
+    if (sMap[key] !== tMap[key]) return false;
   }
   return true;
 };
+
+// some times interviwers assumes sorting takes O(1) times
+// var isAnagram = function (s, t) {
+//   if (s.length !== t.length) return false;
+//   const sortedS = [...s].sort();
+//   const sortedT = [...t].sort();
+//   for (let index = 0; index < sortedS.length; index++) {
+//     if (sortedS[index] !== sortedT[index]) return false;
+//   }
+//   return true;
+// };
 
 console.log(isAnagram("anagram", "nagaram"));
 // isAnagram("anagram", "nagaram");
