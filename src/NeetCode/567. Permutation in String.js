f314ccb8b74fbb93359b6d622cae7c87ad294f92
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-08-08 19:38:42 +0600

    567. Permutation in String ( bare bone )

---
+++ src/NeetCode/567. Permutation in String.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {};
+
+console.log("ab", "eidbaooo");
+// Input: s1 = "ab", s2 = "eidbaooo"
+// Output: true
+// Explanation: s2 contains one permutation of s1 ("ba").
+
+console.log("ab", "eidboaoo");
+// Input: (s1 = "ab"), (s2 = "eidboaoo");
+// Output: false;
+
+// Constraints:
+
+// 1 <= s1.length, s2.length <= 104
+// s1 and s2 consist of lowercase English letters.
