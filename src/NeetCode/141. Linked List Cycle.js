38d1a12c86a1f052d02b7505bb59b74ea92a12eb
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-09-09 20:07:28 +0600

    141. Linked List Cycle

---
+++ src/NeetCode/141. Linked List Cycle.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
+};
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+// Input: head = [1,2], pos = 0
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+// Input: head = [1], pos = -1
+// Output: false
+// Explanation: There is no cycle in the linked list.
+
+// Constraints:
+
+//     The number of the nodes in the list is in the range [0, 104].
+//     -105 <= Node.val <= 105
+//     pos is -1 or a valid index in the linked-list.
+
+// Follow up: Can you solve it using O(1) (i.e. constant) memory?
