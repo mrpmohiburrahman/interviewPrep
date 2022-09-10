c1d103fde91d90d1c79b1935bc7a5c67e1dbbd6b
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-09-10 12:34:53 +0600

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ src/NeetCode/235. Lowest Common Ancestor of a Binary Search Tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  let current = root;
+  while (current) {
+    if (p.val < current.val && q.val < current.val) {
+      current = current.left;
+    } else if (p.val > current.val && q.val > current.val) {
+      current = current.right;
+    } else return current;
+  }
+};
+
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+// Output: 6
+// Explanation: The LCA of nodes 2 and 8 is 6.
+
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+// Output: 2
+// Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+// Input: root = [2,1], p = 2, q = 1
+// Output: 2
+
+// Constraints:
+
+//     The number of nodes in the tree is in the range [2, 105].
+//     -109 <= Node.val <= 109
+//     All Node.val are unique.
+//     p != q
+//     p and q will exist in the BST.
