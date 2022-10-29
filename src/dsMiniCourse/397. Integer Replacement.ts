472e2ed94021c8486bd1551c783d0edac15be719
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-10-29 12:44:53 +0600

    397. Integer Replacement with general recursion

--- src/dsMiniCourse/397. Integer Replacement.ts
+++ src/dsMiniCourse/397. Integer Replacement.ts
@@ -1,20 +1,30 @@
-function integerReplacement(number: number): number {
-  let cache: any = {};
-  const recurReplace = (n: number) => {
-    if (n === 1) return 0;
-    if (n in cache && cache[n] !== undefined) return cache[n];
-    if (n % 2 === 0) {
-      cache[n / 2] = integerReplacement(n / 2);
-      return 1 + cache[n / 2];
-    }
-    cache[n + 1] = 1 + integerReplacement(n + 1);
-    cache[n - 1] = 1 + integerReplacement(n - 1);
+function integerReplacement(n: number): number {
+  if (n === 1) return 0;
+  if (n % 2 === 0) return 1 + integerReplacement(n / 2);
+  let add1 = 1 + integerReplacement(n + 1);
+  let minus1 = 1 + integerReplacement(n - 1);
 
-    cache[n] = Math.min(cache[n + 1], cache[n - 1]);
-    return cache[n];
-  };
-  return recurReplace(number);
+  return Math.min(add1, minus1);
 }
+
+// memoization
+// function integerReplacement(number: number): number {
+//     let cache: any = {};
+//     const recurReplace = (n: number) => {
+//       if (n === 1) return 0;
+//       if (n in cache && cache[n] !== undefined) return cache[n];
+//       if (n % 2 === 0) {
+//         cache[n / 2] = integerReplacement(n / 2);
+//         return 1 + cache[n / 2];
+//       }
+//       cache[n + 1] = 1 + integerReplacement(n + 1);
+//       cache[n - 1] = 1 + integerReplacement(n - 1);
+
+//       cache[n] = Math.min(cache[n + 1], cache[n - 1]);
+//       return cache[n];
+//     };
+//     return recurReplace(number);
+//   }
 
 console.log(integerReplacement(8));
 // Input: n = 8
