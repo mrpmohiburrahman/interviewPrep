88a22a112f33771c239276e7156a00f7450cb81f
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-10-29 12:43:47 +0600

    397. Integer Replacement

---
+++ src/dsMiniCourse/397. Integer Replacement.ts
@@ -0,0 +1,36 @@
+function integerReplacement(number: number): number {
+  let cache: any = {};
+  const recurReplace = (n: number) => {
+    if (n === 1) return 0;
+    if (n in cache && cache[n] !== undefined) return cache[n];
+    if (n % 2 === 0) {
+      cache[n / 2] = integerReplacement(n / 2);
+      return 1 + cache[n / 2];
+    }
+    cache[n + 1] = 1 + integerReplacement(n + 1);
+    cache[n - 1] = 1 + integerReplacement(n - 1);
+
+    cache[n] = Math.min(cache[n + 1], cache[n - 1]);
+    return cache[n];
+  };
+  return recurReplace(number);
+}
+
+console.log(integerReplacement(8));
+// Input: n = 8
+// Output: 3
+// Explanation: 8 -> 4 -> 2 -> 1
+
+console.log(integerReplacement(7));
+// Input: n = 7
+// Output: 4
+// Explanation: 7 -> 8 -> 4 -> 2 -> 1
+// or 7 -> 6 -> 3 -> 2 -> 1
+
+console.log(integerReplacement(4));
+// Input: n = 4
+// Output: 2
+
+// Constraints:
+
+//     1 <= n <= 231 - 1
